246cf9b068ba0bbeb3e2f73339da0b9ba286371c
antth <antth@hotmail.fi>
2020-11-27 14:14:08 +0200

    undo

--- wsgi.py
+++ wsgi.py
@@ -104,18 +104,6 @@ app.layout = html.Div(children=[
         html.H2(children='Frequency plot'),
         dcc.Graph(
             id='bar-plot',
-            config={
-                'modeBarButtonsToRemove': [
-                    'sendDataToCloud',
-                    'lasso2d',
-                    'zoom2d',
-                    'zoomIn2d',
-                    'zoomOut2d',
-                    'pan2d',
-                    'autoScale2d',
-                    'resetScale2d',
-                ]
-            },
         ),
     ]),
 
